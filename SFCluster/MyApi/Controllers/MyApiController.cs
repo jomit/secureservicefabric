4024d198c2a7918b1822a30abec624ac2b140266
Jomit <jovagh@microsoft.com>
2016-10-01 11:53:56 -0700

    added templates for enabling reverse proxy on service fabric cluster added sample app to test the proxy

---
+++ SFCluster/MyApi/Controllers/MyApiController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MyApi.Controllers
+{
+    [ServiceRequestActionFilter]
+    [RoutePrefix("api/v1/myapi")]
+    public class MyApiController : ApiController
+    {
+        [HttpGet]
+        [Route("getdata")]
+        public IEnumerable<string> GetData()
+        {
+            return new string[] { "MyData1", "MyData2" };
+        }
+    }
+}
---
+++ SFCluster/MyApi/ServiceRequestActionFilter.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace MyApi
+{
+    internal class ServiceRequestActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            ServiceEventSource.Current.ServiceRequestStart(actionContext.ActionDescriptor.ActionName);
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            ServiceEventSource.Current.ServiceRequestStop(actionExecutedContext.ActionContext.ActionDescriptor.ActionName,
+                actionExecutedContext.Exception?.ToString() ?? string.Empty);
+        }
+    }
+}
