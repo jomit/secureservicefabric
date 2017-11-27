1d89ad8a2ca63ba6f85004fa5366d9f07342b11e
Jomit <jovagh@microsoft.com>
2017-11-27 11:03:19 -0800

    name changes to redeploy

--- BTest/ScaleScripts.ps1
+++ BTest/ScaleScripts.ps1
@@ -1,12 +1,12 @@
 az login
 
-az group create --name jomitsftest --location westus
+az group create --name jomitsfnew --location westus
 
-az group deployment create --name initial-5-nodes --resource-group jomitsftest --template-file template.json --parameters parameters.json
+az group deployment create --name initial-5-nodes --resource-group jomitsfnew --template-file template.json --parameters parameters.json
 
 # To SCALE UP Primary VMSS -> Change the capacity in the VMSS and and run the deployment again
 
-az group deployment create --name scale-9-nodes --resource-group jomitsftest --template-file template.json --parameters parameters.json
+az group deployment create --name scale-9-nodes --resource-group jomitsfnew --template-file template.json --parameters parameters.json
 
 # To SCALE DOWN Primary VMSS -> run following commands as per the documentation:
 # Documentation : https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-cluster-scale-up-down
@@ -26,7 +26,7 @@ sfctl node disable --node-name "_main_8" --deactivation 4
 
 sfctl node info --node-name "_main_8"
 
-az group deployment create --name scale-8-nodes --resource-group jomitsftest --template-file template.json --parameters parameters.json
+az group deployment create --name scale-8-nodes --resource-group jomitsfnew --template-file template.json --parameters parameters.json
 
 
 
