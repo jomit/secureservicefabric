fb71e5b2280f8009548af8ad1c9c8405f2226a57
Jomit <jovagh@microsoft.com>
2019-02-08 17:41:21 -0800

    added initial sample app for multi tenant app demo

---
+++ MultipleNamedAppInstances/BackendService.Domain/IBackendService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+[assembly: FabricTransportServiceRemotingProvider(RemotingListenerVersion = RemotingListenerVersion.V2, RemotingClientVersion = RemotingClientVersion.V2)]
+namespace BackendService.Domain
+{
+    public interface IBackendService : IService
+    {
+        Task<List<string>> GetPlanetsAsync(CancellationToken cancelToken);
+
+        Task AddPlanetAsync(string name);
+    }
+}
---
+++ MultipleNamedAppInstances/WebService/WebService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Data;
+using System.Net.Http;
+
+namespace WebService
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class WebService : StatelessService
+    {
+        public WebService(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        // in this sample, tenant application names always have the form "fabric:/WebService/<TenantName>
+                        // This extracts the tenant name from the application name and uses it as the web application path.
+
+                        //string tenantName = new Uri(serviceContext.CodePackageActivationContext.ApplicationName).Segments.Last();
+                        //url += $"/{tenantName}";
+
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton<HttpClient>(new HttpClient())
+                                            .AddSingleton<FabricClient>(new FabricClient())
+                                            .AddSingleton<StatelessServiceContext>(serviceContext))
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+
+        internal static Uri GetBackendServiceName(ServiceContext context)
+        {
+            return new Uri($"{context.CodePackageActivationContext.ApplicationName}/BackendService");
+        }
+    }
+}
